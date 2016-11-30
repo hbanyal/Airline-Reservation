7d868cbfb46ac98ad173f30d7989e3f885780b8c
hitendra <hitendrabanyal@gmail.com>
2016-11-30 16:01:18 +0530

    asdsad

--- book/src/main/java/com/book/component/Sender.java
+++ book/src/main/java/com/book/component/Sender.java
@@ -2,49 +2,60 @@ package com.book.component;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @RefreshScope
 @Component
-@EnableBinding(BookingSource.class)
+// @EnableBinding(BookingSource.class)
 public class Sender {
-	private static final Logger logger = LoggerFactory
-			.getLogger(Sender.class);
+	private static final Logger logger = LoggerFactory.getLogger(Sender.class);
 
 	public Sender() {
 
 	}
 
-	/**
-	 * RabbitMessagingTemplate template;
-	 * 
-	 * @Autowired Sender(RabbitMessagingTemplate template){ this.template =
-	 *            template; }
-	 * @Bean Queue queue() { return new Queue("InventoryQ", false); }
-	 * @Bean Queue queue1() { return new Queue("CheckInQ", false); }
-	 **/
+	RabbitMessagingTemplate template;
 
-	@Output(BookingSource.InventoryQ)
 	@Autowired
-	private MessageChannel messageChannel;
+	Sender(RabbitMessagingTemplate template) {
+		this.template = template;
+	}
+
+	@Bean
+	Queue queue() {
+		return new Queue("InventoryQ", false);
+	}
+
+	@Bean
+	Queue queue1() {
+		return new Queue("CheckInQ", false);
+	}
+
+	// @Output(BookingSource.InventoryQ)
+	// @Autowired
+	// private MessageChannel messageChannel;
 
 	public void send(Object message) {
-		// template.convertAndSend("InventoryQ", message);
-		boolean result = messageChannel.send(MessageBuilder.withPayload(message).build());
-		logger.info("booking event successfully delivered: " + result);
+		template.convertAndSend("InventoryQ", message);
+		// boolean result = messageChannel.send(MessageBuilder
+		// .withPayload(message).build());
+		logger.info("booking event successfully delivered: ");
 	}
 }
 
-interface BookingSource {
-	public static String InventoryQ = "inventoryQ";
-
-	@Output("inventoryQ")
-	public MessageChannel inventoryQ();
-
-}
+// interface BookingSource {
+// public static String InventoryQ = "inventoryQ";
+//
+// @Output("inventoryQ")
+// public MessageChannel inventoryQ();
+//
+// }
--- search/src/main/java/com/search/component/Receiver.java
+++ search/src/main/java/com/search/component/Receiver.java
@@ -4,15 +4,18 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.stereotype.Component;
 
 @Component
-@EnableBinding(SearchSink.class)
+//@EnableBinding(SearchSink.class)
 public class Receiver {
 
 	private static final Logger logger = LoggerFactory
@@ -25,35 +28,38 @@ public class Receiver {
 
 	}
 
-	/**
-	 * public Receiver(SearchComponent searchComponent){ this.searchComponent =
-	 * searchComponent; }
-	 * 
-	 * @Bean Queue queue() { return new Queue("InventoryQ", false); }
-	 * @RabbitListener(queues = "InventoryQ") public void
-	 *                        processMessage(Map<String,Object> fare) {
-	 *                        searchComponent
-	 *                        .updateInventory((String)fare.get("FLIGHT_NUMBER"
-	 *                        ),(String)fare.get("FLIGHT_DATE"),(int)fare.get(
-	 *                        "NEW_INVENTORY")); //call repository and update
-	 *                        the fare for the given flight }
-	 **/
+	public Receiver(SearchComponent searchComponent) {
+		this.searchComponent = searchComponent;
+	}
 
-	@ServiceActivator(inputChannel = SearchSink.INVENTORYQ)
-	public void accept(Map<String, Object> fare) {
+	@Bean
+	Queue queue() {
+		return new Queue("InventoryQ", false);
+	}
+
+	@RabbitListener(queues = "InventoryQ")
+	public void processMessage(Map<String, Object> fare) {
 		logger.info("Update inventory received: ");
 		searchComponent.updateInventory((String) fare.get("FLIGHT_NUMBER"),
 				(String) fare.get("FLIGHT_DATE"),
 				(int) fare.get("NEW_INVENTORY"));
-		logger.info("Update inventory received: "
-				+ (int) fare.get("NEW_INVENTORY"));
 	}
+
+//	@ServiceActivator(inputChannel = SearchSink.INVENTORYQ)
+//	public void accept(Map<String, Object> fare) {
+//		logger.info("Update inventory received: ");
+//		searchComponent.updateInventory((String) fare.get("FLIGHT_NUMBER"),
+//				(String) fare.get("FLIGHT_DATE"),
+//				(int) fare.get("NEW_INVENTORY"));
+//		logger.info("Update inventory received: "
+//				+ (int) fare.get("NEW_INVENTORY"));
+//	}
 }
 
-interface SearchSink {
-	public static String INVENTORYQ = "inventoryQ";
-
-	@Input("inventoryQ")
-	public MessageChannel inventoryQ();
-
-}
+//interface SearchSink {
+//	public static String INVENTORYQ = "inventoryQ";
+//
+//	@Input("inventoryQ")
+//	public MessageChannel inventoryQ();
+//
+//}
